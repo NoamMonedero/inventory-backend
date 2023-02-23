0f71355f1cce24902725eff844e403c0cb637173
NoamMonederoCasa <noammome98@gmail.com>
2023-02-23 20:22:22 +0100

    servicio eliminar cateǵoría por id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,10 +59,28 @@ public class CategoryRestController {
 		return response;
 	}
 	
+	/**
+	 * update categories
+	 * @param category
+	 * @param id
+	 * @return
+	 */
 	@PutMapping("/categories/{id}")
 	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id){
 		
 		ResponseEntity<CategoryResponseRest> response = service.update(category, id);
+		return response;
+	}
+	
+	/**
+	 * delete category
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> delete(@PathVariable Long id){
+		
+		ResponseEntity<CategoryResponseRest> response = service.deleteById(id);
 		return response;
 	}
 }
