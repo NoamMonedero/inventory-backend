4f03f9a78f79abd00727278594687278e2bb857c
NoamMonederoCasa <noammome98@gmail.com>
2023-03-11 14:04:00 +0100

    buscar productos

--- src/main/java/com/company/inventory/controller/ProductRestController.java
+++ src/main/java/com/company/inventory/controller/ProductRestController.java
@@ -91,4 +91,14 @@ public class ProductRestController {
 		ResponseEntity<ProductResponseRest> response = productService.deleteById(id);
 		return response;
 	}
+	
+	/**
+	 * search
+	 * @return
+	 */
+	@GetMapping("/products")
+	public ResponseEntity<ProductResponseRest> search(){
+		ResponseEntity<ProductResponseRest> response = productService.search();
+		return response;
+	}
 }
